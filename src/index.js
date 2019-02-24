1c0f7da1ba9bd7e6d2aa40d457a98f32999ad136
Liu Ren <liuren@cc1st.com>
2019-02-24 16:03:28 +0800

    初始化

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
+import logger from 'redux-logger';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import {createStore, applyMiddleware} from 'redux';
+import rootReducer from './reducers';
+import { Provider } from 'react-redux';
+import App from './App';
 
-console.log(React);
-console.log(ReactDOM);
+const store = createStore(
+    rootReducer,
+);
+
+render(
+    <Provider store={store}>
+      <App/>
+    </Provider>,
+    document.querySelector('#root'),
+);
